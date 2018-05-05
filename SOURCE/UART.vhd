bd03d4d69a0c4394332e1a3a467f28ea48947bc9
Unknown <LTMayhew@Hotmail.com>
2018-05-05 19:40:52 -0400

    Added FIFO

--- SOURCE/UART.vhd
+++ SOURCE/UART.vhd
@@ -36,7 +36,7 @@ entity UART is
            reset : in std_logic;
            rx_in : in STD_LOGIC;
            tx_out : out STD_LOGIC;
-           rx_done,tx_done : out std_logic;
+           rx_read : in std_logic;
            tx_start : in std_logic;
            data_out : out STD_LOGIC_VECTOR (7 downto 0);
            data_in : in STD_LOGIC_VECTOR (7 downto 0));
@@ -87,17 +87,38 @@ architecture Behavioral of UART is
                 state : out std_logic_vector(1 downto 0));
     end component;
     
+    component FIFO
+        Generic(
+            DATA_WIDTH : INTEGER := 8;
+            MAX_HEIGHT : INTEGER := 16
+        );
+    
+        Port (
+            rst : in STD_LOGIC;
+            clk : in STD_LOGIC;
+            wr_en : in STD_LOGIC;
+            rd_en : in STD_LOGIC;
+            wr_data : in STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
+            rd_data : out STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
+            empty : out STD_LOGIC;
+            full : out STD_LOGIC
+        );
+    end component;
   signal max_tick_temp : std_logic;
   signal tick_out_temp : std_logic_vector(9 downto 0);  
   signal dout_temp,din_temp : std_logic_vector (7 downto 0);
   signal RX_state,TX_state : std_logic_vector (1 downto 0);
+  signal rx_done_temp,tx_done_temp : std_logic;
+  signal empty_temp,full_temp : std_logic;
   
 begin
 BG : BaudGen 
     port map (clk=>clk, reset=>reset,max_tick=>max_tick_temp,tick_out=>tick_out_temp); 
 RX : Receiver 
-    port map (clk=>clk, rst=>reset,rx=>rx_in,s_tick=>max_tick_temp,rx_done_tick=>rx_done,dout=>dout_temp,state_out=>RX_state);
+    port map (clk=>clk, rst=>reset,rx=>rx_in,s_tick=>max_tick_temp,rx_done_tick=>rx_done_temp,dout=>dout_temp,state_out=>RX_state);
 TX : Transmitter
-    port map (clk=>clk, reset=>reset,tx_start=>tx_start,s_tick=>max_tick_temp,din=>din_temp,tx_done_tick=>tx_done,tx=>tx_out,state=>tx_state);
-
+    port map (clk=>clk, reset=>reset,tx_start=>tx_start,s_tick=>max_tick_temp,din=>data_in,tx_done_tick=>tx_done_temp,tx=>tx_out,state=>tx_state);
+RXFIFO : FIFO
+    port map (clk=>clk, rst=>reset,wr_en=>rx_done_temp,rd_en=>rx_read,wr_data=>dout_temp,rd_data=>data_out,empty=>empty_temp,full=>full_temp);
+    
 end Behavioral;
